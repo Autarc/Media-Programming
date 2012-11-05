6c172d1297f6c835d561f94996966da2fb10fee7
Autarc <Autarc@gmail.com>
2012-11-05 17:10:24 +0100

    scaffold pgmbin.c

---
+++ Exercise 2/pgmbin.c
@@ -0,0 +1,32 @@
+/*
+	A c command line tool which reads PGM-images (binary or ascii).
+
+	Use -help to learn more about the options.
+
+	: comments
+	: width
+	: height
+	: filename or -help
+
+	: statistic values =>	arithemic mean, the median, the probability of the colors, modal value,
+							maximum, minimum
+
+	@params
+
+	argv[2] => filename or -help
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// define type
+typedef unsigned char byte;
+
+
+int main ( int argc, char * argv[] ) {
+
+	return 0;
+}
